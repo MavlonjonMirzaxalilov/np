f89de892b3cf99fb6b4a9c74def9dc72b0709abb
MavlonjonMirzaxalilov <mirzaxalilovmavlonjon7@gmail.com>
2024-11-02 12:00:02 +0500

    start project

---
+++ app/dashboard/analytics/page.tsx
@@ -0,0 +1,11 @@
+
+
+const Page = () => {
+	return (
+		<div>
+			Analytics
+		</div>
+	)
+}
+
+export default Page
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Hello from './components/Hello'
+
+export default function Home() {
+	return (
+		<>
+			<h1 className='text-3xl'>Welcome to next js</h1>
+      <Hello/>
+		</>
+	)
+}
